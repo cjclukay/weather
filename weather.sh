194d9b9668df5fef41a13d30d86868fc2cdbf82a
Christopher Clukay <cjclukay@i21b-s1.ufhpc>
2016-09-16 11:38:32 -0400

    Added Airport Option

--- weather.sh
+++ weather.sh
@@ -6,9 +6,9 @@ month=`date -d yesterday +%m`
 
 year=`date -d yesterday +%Y`
 
-curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.html?&format=1" >> gnv.txt
+curl "https://www.wunderground.com/history/airport/$1/$year/$month/$day/DailyHistory.html?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
-echo The Max temp is $maxTemp
+echo The Max temp at $1 is $maxTemp
 
